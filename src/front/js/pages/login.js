3a6cafad2f3896b788603c77e1d3f1bfa675b894
Yohan <102123553+yohanmayorga@users.noreply.github.com>
2023-10-12 19:54:25 +0000

    added sign in HTML

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -14,6 +14,8 @@ export const Login = () => {
 
         const { store, actions } = useContext(Context);
 
+        const [isShow, setIsShown] = useState(false);
+
         async function addUser() {
                 if (user.password == user.password_check) {
                         let created = await actions.createUser(user);
@@ -22,41 +24,50 @@ export const Login = () => {
                         console.log("error")
                 }
         }
-        
+
         return (
-                <form className="Login">
+                <div className="Login">
+                        {!isShow ? <h1>Login</h1> : <h1>Sign up</h1>}
+                        <div>
+                                <button className="FormButton" onClick={() => setIsShown(false)}>Log In</button>
+                                <button className="FormButton" onClick={() => setIsShown(true)}>Sign up</button>
+                        </div>
+                        <form className="Form">
 
-                        <label for="email">Email: </label>
-                        <input
-                                type="text"
-                                name="email"
-                                id="email"
-                                placeholder="Email"
-                                onChange={(e) => setUser({ ...user, email: e.target.value })}
-                                required
-                        />
+                                <label for="email">Email: </label>
+                                <input
+                                        type="text"
+                                        name="email"
+                                        id="email"
+                                        placeholder="Email"
+                                        onChange={(e) => setUser({ ...user, email: e.target.value })}
+                                        required
+                                />
 
-                        <label for="password">Password: </label>
-                        <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                placeholder="Password"
-                                onChange={(e) => setUser({ ...user, password: e.target.value })}
-                                required />
+                                <label for="password">Password: </label>
+                                <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="Password"
+                                        onChange={(e) => setUser({ ...user, password: e.target.value })}
+                                        required />
 
+                                {isShow && (
+                                        <>
+                                                <label for="password_check">Repeat Password: </label>
+                                                <input
+                                                        type="password"
+                                                        name="password_check"
+                                                        id="password_check"
+                                                        placeholder="Repeat Password"
+                                                        onChange={(e) => setUser({ ...user, password_check: e.target.value })}
+                                                        required />
+                                        </>
+                                )}
 
-                        <label for="password_check">Password: </label>
-                        <input
-                                type="password"
-                                name="password_check"
-                                id="password_check"
-                                placeholder="Repeat Password"
-                                onChange={(e) => setUser({ ...user, password_check: e.target.value })}
-                                required />
-
-
-                        <input type="submit" value="Register" onClick={() => addUser()} />
-                </form>
+                                <input type="submit" value={!isShow ? "Login" : "Sign up"} onClick={() => addUser()} />
+                        </form>
+                </div>
         )
 };
